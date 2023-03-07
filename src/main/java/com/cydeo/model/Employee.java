2796d927101dc91e7d4e5e6e17b454b7be691c40
shafiq bayat <bayat37@hotmail.com>
2023-03-07 13:13:30 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/model/Employee.java
@@ -0,0 +1,49 @@
+package com.cydeo.model;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+@ToString
+public class Employee {
+
+//    @NotNull        --->    Field shouldn't be null
+//    @NotEmpty       --->    Field shouldn't be ""
+//    @NotBlank       --->    Field shouldn't be "       "
+
+//    @NotNull -> @NotNull
+//    @NotEmpty -> @NotNull + @NotEmpty
+//    @NotBlank -> @NotNull + @NotEmpty + @NotBlank
+
+    @NotBlank
+    @Size(max = 12, min = 2)
+    private String firstName;
+
+//    @NotBlank
+//    @Size(max = 12, min = 2)
+    private String lastName;
+
+    // Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy  -   yyyy-dd-mm
+
+//    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+
+//    @NotBlank
+//    @Email
+    private String email;
+
+//    @NotBlank
+//    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
+    private String password;
+
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
